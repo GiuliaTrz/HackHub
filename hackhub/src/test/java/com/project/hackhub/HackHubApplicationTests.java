191f7cca616ce15d2807addd7beed1aba991869f
PeaceLily <123887125+Sunlight-note@users.noreply.github.com>
2026-03-18 17:19:57 +0100

    Update and rename HackhubApplicationTests.java to HackHubApplicationTests.java

--- hackhub/src/test/java/com/project/hackhub/HackHubApplicationTests.java
+++ hackhub/src/test/java/com/project/hackhub/HackHubApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HackhubApplicationTests {
+class HackHubApplicationTests {
 
 	@Test
 	void contextLoads() {
