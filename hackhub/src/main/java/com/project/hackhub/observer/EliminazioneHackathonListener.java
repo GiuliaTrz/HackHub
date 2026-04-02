48793930a4a58c25dc93f33f92fd909e8d9b3d49
PeaceLily <giorgiabranch01@gmail.com>
2026-04-02 23:31:38 +0200

    updated listeners methods

--- hackhub/src/main/java/com/project/hackhub/observer/EliminazioneHackathonListener.java
+++ hackhub/src/main/java/com/project/hackhub/observer/EliminazioneHackathonListener.java
@@ -1,4 +1,18 @@
 package com.project.hackhub.observer;
 
+import com.project.hackhub.model.utente.UtenteRegistrato;
+
+import java.util.List;
+
 public class EliminazioneHackathonListener implements EventListener{
+    @Override
+    public void updateUsers(List<UtenteRegistrato> usersList, String message, Object entity) {
+        //TODO
+    }
+
+    @Override
+    public EventType getSupportedEventType() {
+        //TODO
+        return null;
+    }
 }
