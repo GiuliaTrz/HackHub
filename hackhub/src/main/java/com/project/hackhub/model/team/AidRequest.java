1c7b0470ac8377bf3d69e0d3ca43821616230681
GiuliaTrz <148765503+GiuliaTrz@users.noreply.github.com>
2026-04-03 22:30:24 +0200

    Added AidRequest

--- hackhub/src/main/java/com/project/hackhub/model/team/AidRequest.java
+++ hackhub/src/main/java/com/project/hackhub/model/team/AidRequest.java
@@ -14,12 +14,36 @@ public class AidRequest {
     @OneToOne
     private Team team;
 
+<<<<<<< Updated upstream
     public AidRequest(Team t, String type){
         this.team = t;
         this.type = type;
+=======
+    public AidRequest(Team t, AidRequestType type, String description){
+        if (t == null || type == null)
+            throw new IllegalArgumentException("Parametri non validi.");
+
+        this.team = t;
+        this.type = type;
+        this.description = description;
+    }
+
+    public AidRequest(Team t, AidRequestType type){
+        this(t, type, null);
+>>>>>>> Stashed changes
     }
 
     public Hackathon getHackathon() {
-        return this.team.getHackathon();
+        if (team == null)
+            throw new IllegalStateException("Team non associato.");
+        return team.getHackathon();
+    }
+<<<<<<< Updated upstream
+}
+=======
+
+    public Team getTeam() {
+        return this.team;
     }
 }
+>>>>>>> Stashed changes
