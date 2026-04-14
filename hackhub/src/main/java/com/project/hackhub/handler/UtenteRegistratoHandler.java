737561ae9a6171c8f2fb6debf3c901c84eed49d6
GiuliaTrz <148765503+GiuliaTrz@users.noreply.github.com>
2026-04-14 15:47:35 +0200

    update: bug fix, TeamHandler

--- hackhub/src/main/java/com/project/hackhub/handler/UtenteRegistratoHandler.java
+++ hackhub/src/main/java/com/project/hackhub/handler/UtenteRegistratoHandler.java
@@ -3,78 +3,95 @@ package com.project.hackhub.handler;
 import com.project.hackhub.model.hackathon.Hackathon;
 import com.project.hackhub.model.hackathon.state.HackathonStateType;
 import com.project.hackhub.model.team.Invito;
+import com.project.hackhub.model.team.Team;
 import com.project.hackhub.model.utente.UtenteRegistrato;
-import com.project.hackhub.model.utente.state.UserState;
-import com.project.hackhub.model.utente.state.UserStateFactory;
 import com.project.hackhub.model.utente.state.UserStateType;
+import com.project.hackhub.repository.InvitoRepository;
 import com.project.hackhub.repository.TeamRepository;
 import com.project.hackhub.repository.UtenteRegistratoRepository;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import static com.project.hackhub.service.UserStateService.changeUserState;
 
-@AllArgsConstructor
+@Component
+@RequiredArgsConstructor
 public class UtenteRegistratoHandler {
 
     private final UtenteRegistratoRepository utenteRegistratoRepo;
     private final TeamRepository teamRepo;
-    private final TeamHandler teamHandler;
+    private final InvitoRepository invitoRepo;
 
+    /**
+     * Aggiunge un invito alla lista dell'utente.
+     * @author Giulia Trozzi
+     */
     public void addInvitation(UtenteRegistrato user, Invito invitation) {
-
         user.addInvitation(invitation);
         utenteRegistratoRepo.save(user);
-
     }
 
     /**
-     * Accept invitation to take part on a team
+     * Accetta un invito a far parte di un team.
      *
-     * @param i the invitation to accept
-     * @throws IllegalArgumentException      if the invitation is {@code null}
-     * @throws UnsupportedOperationException if the {@link Hackathon} is not in {@link HackathonStateType#IN_ISCRIZIONE}
-     *                                       or if the user is not available, so they can't accept the invitation
-     * @author Giorgia Branchesi
+     * @param i l'invito da accettare
+     * @throws IllegalArgumentException      se l'invito è null
+     * @throws UnsupportedOperationException se l'hackathon non è in stato IN_ISCRIZIONE
+     *                                       o se l'utente non è disponibile
+     * @author Giulia Trozzi
      */
     public void acceptInvitation(Invito i) {
-
         if (i == null)
-            throw new IllegalArgumentException("the invitation cannot be null");
+            throw new IllegalArgumentException("The invitation cannot be null");
 
-        UtenteRegistrato addressee = i.getDestinatario();
-        Hackathon h = i.getMittente().getHackathon();
+        UtenteRegistrato destinatario = i.getDestinatario();
+        Team team = i.getMittente();
+        Hackathon hackathon = team.getHackathon();
 
-        if (!h.getState().getStateType().equals(HackathonStateType.IN_ISCRIZIONE)
-                || !addressee.isAvailable(h.getReservation()))
+        if (!hackathon.getState().getStateType().equals(HackathonStateType.IN_ISCRIZIONE)
+                || !destinatario.isAvailable(hackathon.getReservation())) {
             throw new UnsupportedOperationException("Azione non permessa");
+        }
 
-        addressee.removeInvitation(i);
-        changeUserState(addressee, true, h, UserStateType.MEMBRO_DEL_TEAM);
-        teamHandler.acceptInvitation(i);
+        //rimuovi l'invito dalla lista dell'utente
+        destinatario.removeInvitation(i);
+        utenteRegistratoRepo.save(destinatario);
+
+        //cambia lo stato dell'utente in MEMBRO_DEL_TEAM per questo hackathon
+        changeUserState(destinatario, true, hackathon, UserStateType.MEMBRO_DEL_TEAM);
+
+        //al'utente come membro del team
+        team.addTeamMember(destinatario);
+        teamRepo.save(team);
+
+        //elimina l'invito dal database
+        invitoRepo.delete(i);
     }
 
     /**
-     * Declines invitation to take part on a team
+     * Rifiuta un invito a far parte di un team.
      *
-     * @param i the invitation to decline
-     * @throws IllegalArgumentException      if the invitation is {@code null}
-     * @throws UnsupportedOperationException if the {@link Hackathon} is not in {@link HackathonStateType#IN_ISCRIZIONE}
-     * @author Giorgia Branchesi
+     * @param i l'invito da rifiutare
+     * @throws IllegalArgumentException      se l'invito è null
+     * @throws UnsupportedOperationException se l'hackathon non è in stato IN_ISCRIZIONE
+     * @author Giulia Trozzi
      */
     public void declineInvitation(Invito i) {
-
         if (i == null)
-            throw new IllegalArgumentException("the invitation cannot be null");
+            throw new IllegalArgumentException("The invitation cannot be null");
 
-        UtenteRegistrato addressee = i.getDestinatario();
-        Hackathon h = i.getMittente().getHackathon();
+        UtenteRegistrato destinatario = i.getDestinatario();
+        Hackathon hackathon = i.getMittente().getHackathon();
 
-        if (!h.getState().getStateType().equals(HackathonStateType.IN_ISCRIZIONE))
+        if (!hackathon.getState().getStateType().equals(HackathonStateType.IN_ISCRIZIONE)) {
             throw new UnsupportedOperationException("Azione non permessa");
+        }
 
-        addressee.removeInvitation(i);
-        utenteRegistratoRepo.save(addressee);
-        teamHandler.removeInvitation(i);
+        //rimuovi l'invito dalla lista dell'utente
+        destinatario.removeInvitation(i);
+        utenteRegistratoRepo.save(destinatario);
+
+        //elimina l'invito dal database
+        invitoRepo.delete(i);
     }
-
 }
