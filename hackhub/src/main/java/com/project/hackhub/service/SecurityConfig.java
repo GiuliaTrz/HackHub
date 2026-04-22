7f3dde787be6d5205cb3d944b16e45f07c8db8fd
PeaceLily <giorgiabranch01@gmail.com>
2026-04-22 20:02:25 +0200

    user authentication functionality added

---
+++ hackhub/src/main/java/com/project/hackhub/service/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.project.hackhub.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@AllArgsConstructor
+public class SecurityConfig {
+
+    private final JwtFilter filter;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/autenticazione/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/hackathon").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
