3c40d7090ccfb6e07332c0a96e2d2a82ead078af
Chiara Marinucci <chiaramarinucci94@gmail.com>
2026-04-24 22:57:12 +0200

    code for GradeBoundary

---
+++ hackhub/src/main/java/com/project/hackhub/boundary/GradeBoundary.java
@@ -0,0 +1,38 @@
+package com.project.hackhub.boundary;
+
+import com.project.hackhub.dto.GradeDTO;
+import com.project.hackhub.handler.GradeHandler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+
+@RestController
+@RequestMapping("/api/grades")
+public class GradeBoundary {
+    private final GradeHandler gradeHandler;
+
+    public GradeBoundary(GradeHandler gradeHandler) {
+        this.gradeHandler = gradeHandler;
+    }
+
+    /**
+     * Allows a judge to grade a submission by providing the submission id and the grade details.
+     * The judge must be authenticated to perform this action.
+     * @param judge the id of the judge performing the action
+     * @param submissionId the id of the submission to grade
+     * @param dto the record containing the grade
+     * @return a ResponseEntity with a success message if the grading is successful
+     * @author Chiara Marinucci
+     */
+    @PatchMapping("/submission/{submissionId}")
+    public ResponseEntity<String> gradeSubmission(
+            @AuthenticationPrincipal UUID judge,
+            @PathVariable UUID submissionId,
+            @RequestBody GradeDTO dto){
+        gradeHandler.gradeSubmission(judge, submissionId, dto);
+        return ResponseEntity.ok("submission " + submissionId + " successfully graded: " + dto.grade());
+    }
+}
---
+++ hackhub/src/main/java/com/project/hackhub/dto/GradeDTO.java
@@ -0,0 +1,4 @@
+package com.project.hackhub.dto;
+
+public record GradeDTO(float grade) {
+}
