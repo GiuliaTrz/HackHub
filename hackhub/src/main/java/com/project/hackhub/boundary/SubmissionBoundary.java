e9c0972b5ea6fc4411d41bb15bda079ae3b007ee
Chiara Marinucci <chiaramarinucci94@gmail.com>
2026-04-24 17:50:37 +0200

    fixed getTeamSubmissions and added javadoc

--- hackhub/src/main/java/com/project/hackhub/boundary/SubmissionBoundary.java
+++ hackhub/src/main/java/com/project/hackhub/boundary/SubmissionBoundary.java
@@ -20,6 +20,15 @@ public class SubmissionBoundary {
         this.submissionHandler = submissionHandler;
     }
 
+    /**
+     * Allows user to send a submission associated to a team for a given task
+     * @param teamLeader id of the user
+     * @param team id of the team
+     * @param task if of the task
+     * @param fileTemplate the file template containing the submission file
+     * @return a ResponseEntity with status CREATED if successful
+     * @author Chiara Marinucci
+     */
     @PostMapping("/{teamId}/{taskId}")
     public ResponseEntity<Void> sendSubmission(
             @AuthenticationPrincipal UUID teamLeader,
@@ -30,12 +39,19 @@ public class SubmissionBoundary {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-
+    /**
+     * Allows a user to view the list of a team's submission according to user's permissions.
+     * Staff can view all teams' submissions, team members only their own.
+     * @param user the id of the user
+     * @param teamId the id of the team
+     * @return a list of the team's submissions if the user has the necessary permission.
+     * @author Chiara Marinucci
+     */
     @GetMapping("/team/{teamId}")
     public ResponseEntity<List<Submission>> getTeamSubmissions(
-            @AuthenticationPrincipal UUID user, UUID team,
-            @PathVariable String teamId){
-            List<Submission> submissions = submissionHandler.getTeamSubmissions(user, team);
+            @AuthenticationPrincipal UUID user,
+            @PathVariable UUID teamId){
+            List<Submission> submissions = submissionHandler.getTeamSubmissions(user, teamId);
             if(submissions.isEmpty())
                 return ResponseEntity.noContent().build(); //204
             return ResponseEntity.ok(submissions); //200 OK
