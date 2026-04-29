2a5fb9678cf123a52fc1b3326aabb4598ac1bf52
GiuliaTrz <148765503+GiuliaTrz@users.noreply.github.com>
2026-04-29 18:21:34 +0200

    fix-added-listener

--- hackhub/src/main/java/com/project/hackhub/observer/RimozioneDaTeamListener.java
+++ hackhub/src/main/java/com/project/hackhub/observer/RimozioneDaTeamListener.java
@@ -1,18 +1,34 @@
 package com.project.hackhub.observer;
 
+import com.project.hackhub.model.hackathon.Hackathon;
 import com.project.hackhub.model.utente.UtenteRegistrato;
+import com.project.hackhub.model.utente.state.UserStateType;
+import com.project.hackhub.service.UserStateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-public class RimozioneDaTeamListener implements EventListener{
+@Component
+@RequiredArgsConstructor
+public class RimozioneDaTeamListener implements EventListener {
+
+    private final UserStateService userStateService;
+
     @Override
     public void updateUsers(List<UtenteRegistrato> usersList, String message, Object entity) {
-        //TODO
+        if (!(entity instanceof Hackathon hackathon)) {
+            throw new IllegalArgumentException("Entity must be a Hackathon");
+        }
+
+        for (UtenteRegistrato user : usersList) {
+            // Rimuove lo stato di partecipazione al team riportando a DEFAULT_STATE
+            userStateService.changeUserState(user, false, hackathon, UserStateType.DEFAULT_STATE);
+        }
     }
 
     @Override
     public EventType getSupportedEventType() {
-        //TODO
-        return null;
+        return EventType.RIMOZIONE_DA_TEAM;
     }
 }
